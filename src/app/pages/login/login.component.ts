071544c56a578a967c57488d477fafd40b6d5abf
Robson Maia <robsonmvieira@gmail.com>
2021-05-04 10:11:57 -0300

    :sparkles: update login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildform()
+  }
+
+  buildform(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  submit(): void {
+    console.log(this.loginForm.getRawValue())
   }
 
 }
+// https://coderthemes.com/hyper/saas/index.html

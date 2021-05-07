153eb9e63ac4f1263bfae9d0eb2f71a0716ebfd0
Robson Maia <robsonmvieira@gmail.com>
2021-05-07 14:31:11 -0300

    :sparkles: add httpInterceptor

---
+++ src/app/services/http.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpInterceptorRequest implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken()
+    const request = req.clone({setHeaders: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+  }});
+    return next.handle(request);
+  }
+}
